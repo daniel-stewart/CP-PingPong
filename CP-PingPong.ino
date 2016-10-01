5eb8bdfe5d620c08d12782c4bed549cb3185a213
daniel-stewart <mpc823_99@yahoo.com>
2016-09-30 22:46:21 -0400

    Create CP-PingPong.ino

---
+++ CP-PingPong.ino
@@ -0,0 +1,12 @@
+#include <Adafruit_CircuitPlayground.h>
+
+void setup() {
+  CircuitPlayground.begin();
+  CircuitPlayground.clearPixels();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
